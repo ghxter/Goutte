12b1c34be28355ffbc7a3270db825dfda18d0f34
Rock <ghxter@gmail.com>
2017-09-18 15:52:33 +0800

    add taobao bbs for crawler qq groups

---
+++ Logger.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Logger Tracking
+ *
+ * @author     rock <rock_guo@hexu.org>
+ * @copyright  Copyright (c) 2010-2014 hexu Org. (http://www.hexu.org )
+ * @license    http://www.hexu.org/license/
+ * @version    CVS: $Id: Logger.php,v 1.1 2010/07/23 06:50:11 rock_guo Exp $
+ */
+
+/** define application environment */
+defined('_DNS_TRACKING')  || define('_DNS_TRACKING', 'mysql:host=localhost;dbname=test');
+defined('_DNS_USERNAME')  || define('_DNS_USERNAME', 'root');
+defined('_DNS_PASSWORD')  || define('_DNS_PASSWORD', '123456');
+defined('TABLE_PREFIX') || define('TABLE_PREFIX', 'qq_');
+/**
+ * Tracking_Tracker
+ *
+ * @category   Tracking
+ * @package    Tracking_Tracker
+ */
+class Tracking_Logger {
+
+    /**
+     * @var resource
+     */
+    private $_dbTracking = null;
+    /**
+     * Singleton instance
+     *
+     * Marked only as protected to allow extension of the class. To extend,
+     * simply override {@link getInstance()}.
+     *
+     * @var Tracking_Logger
+     */
+    protected static $_instance = null;
+    /**
+     * Singleton instance
+     *
+     * @return Tracking_Logger
+     */
+    public static function getInstance()
+    {
+        if (null === self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+    /**
+     * Constructor
+     * Tracking_Logger
+     */
+    public function __construct() {
+
+        if(null == $this->_dbTracking){
+            $trkDns = _DNS_TRACKING;
+            //$trkDns = Tracking_Registry::get('dbTracking');
+            $this->_dbTracking = $this->_getConnection($trkDns);
+        }
+    }
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return bool
+     * @throws Exception
+     */
+    public function __call($method, $params = array()) {
+        if(empty($params)) {
+            throw new Exception("Error Info: {$method} params is empty.");
+        }
+        $table = TABLE_PREFIX . strtolower(substr($method, 3)) . '_log';
+
+        $fields = $values = '';
+        $binds = [];
+        foreach (array_shift($params) as $key => $val) {
+            $fields .= ',`' . $key . '`';
+            $values .= ',?';
+            $binds[] = addcslashes(str_replace("'", "''", $val), "\000\n\r\\\032");
+        }
+        $fields  = substr($fields, 1);
+        $values  = substr($values, 1);
+        $sth = $this->_dbTracking->prepare("INSERT INTO {$table} ( {$fields} ) VALUES ( {$values} )");
+        if($sth->execute($binds)) {
+            return true;
+        } else {
+            $errors = $sth->errorInfo();
+            throw new Exception("Error Info: {$errors[2]}.");
+            //error_log($errors[2]);
+        }
+    }
+
+    /**
+     * @desc 取最近一小时的IP访问量
+     * @param $params
+     * @return mixed
+     */
+    public function isExist($params)
+    {
+        $table = TABLE_PREFIX . "groups_log";
+        $sth = $this->_dbTracking->prepare("SELECT COUNT(*) cnt FROM $table WHERE `detail_url` = ?");
+
+        $sth->execute([$params['detail_url']]);
+        $result = $sth->fetch(PDO::FETCH_ASSOC);
+        //error_log($currentTime . '====' . $startTime, 0);
+
+        return !empty($result['cnt']) ? $result['cnt'] : 0;
+    }
+    /**
+     * @desc get front end mysql connection
+     *
+     * @param null $dsn
+     * @return bool|PDO
+     */
+    private function _getConnection($dsn = null) {
+        //suppress error
+        if (null == $dsn) {
+            return FALSE;
+        }
+        $options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"];
+
+//        if (!empty($dsn->charset)) {
+//            $initCommand = "SET NAMES '" . $dsn->charset . "'";
+//            $options[PDO::MYSQL_ATTR_INIT_COMMAND] = $initCommand; // 1002 = PDO::MYSQL_ATTR_INIT_COMMAND
+//        }
+        // var_dump("mysql:host={$dsn->host};dbname={$dsn->dbname}", $dsn->username, $dsn->password, $options);exit;
+        // $conn = new PDO("mysql:host={$dsn->host};dbname={$dsn->dbname}", $dsn->username, $dsn->password, $options);
+        $conn = new PDO($dsn, _DNS_USERNAME, _DNS_PASSWORD, $options);
+        return $conn;
+
+    }
+}
---
+++ taobao.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Taobao
+ *
+ * @author     rock <rock_guo@hexu.org>
+ * @copyright  Copyright (c) 2010-2014 hexu Org. (http://www.hexu.org )
+ * @license    http://www.hexu.org/license/
+ * @version    CVS: $Id: taobao.php,v 1.1 2017/09/18 11:50:11 rock_guo Exp $
+ */
+
+
+require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/Logger.php";
+
+use Goutte\Client;
+
+//
+//$qqs = ["新手淘交流q群602548967",
+//    "新手淘交流qq群602548967",
+//    "新手淘交流qq群:602548961新手淘交流qq群:602548967新手淘交流qq群:602548962新手淘交流qq群:602548963新手淘交流qq群:602548964",
+//    "新手淘交流qq群：602548967",
+//    "新手淘交流qq群:602548961新手淘交流新手淘交流qq群：602548962新手淘交流qq群新手淘交流qq群：602548963新手淘交流qq群",
+//    "新手淘交流qq群：602548967新手淘交流qq群新手淘交流qq群：602548962新手淘交流qq群新手淘交流qq群：602548963新手淘交流qq群"
+//];
+//foreach($qqs as $q) {
+//    echo $q . PHP_EOL;
+//    preg_match_all("/((q群|qq群|qq|q)[^0-9]*([0-9]+))/i", $q, $qqs);
+//    print_r($qqs);
+//
+//}
+//
+//exit;
+
+$client = new Client();
+//$logger = Tracking_Logger::getInstance()->logGroups();
+
+for ($i = 1; $i < 1000; $i++) {
+    $listUrl = "https://maijia.bbs.taobao.com/search.html?spm=a210m.7699124.0.0.5626320fBQ2iHL&keyword=qq%E7%BE%A4&page=" . $i;
+
+    $crawler = $client->request("GET", $listUrl);
+    $html = $crawler->html();
+
+    preg_match_all("/<a.*href=\"(\/detail[^\"]+)\"[^>]+>(.*)<\/a>/i", $html, $matches);
+    $html = $crawler = null;
+
+    if (empty($matches[1])) {
+        continue;
+    }
+    foreach ($matches[1] as $detailUrl) {
+        $detailUrl = "https://maijia.bbs.taobao.com" . $detailUrl;
+        /**
+         * check is crawler-ed
+         */
+        if(Tracking_Logger::getInstance()->isExist(['detail_url' => $detailUrl])) {
+            echo "[Notice]: detail url is crawler-ed `{$detailUrl}` \n";
+            continue;
+        }
+
+        $crawler = $client->request("GET", $detailUrl);
+        $html = $crawler->text();
+
+        preg_match_all("/.*((q群|qq群|qq|q)[^0-9]*([0-9]{7,})).*/i", $html, $qqs);
+
+        if (!empty($qqs[3])) {
+            $qqGroups = array_count_values($qqs[3]);
+            $qqDescriptions = str_replace(["\t", "\n", " "], "", implode("#####", array_keys(array_count_values($qqs[0]))));
+
+            foreach ($qqGroups as $group => $times) {
+                /**
+                 * insert into db
+                 */
+                $logGroupsParams = [
+                    'list_url' => $listUrl,
+                    'detail_url' => $detailUrl,
+                    'group_num' => $group,
+                    'group_times' => $times,
+                    "description" => $qqDescriptions,
+                ];
+
+                print_r($logGroupsParams);
+
+                try {
+                    Tracking_Logger::getInstance()->logGroups($logGroupsParams);
+                } catch (Exception $e) {
+                    echo $e->getMessage();
+                }
+            }
+
+        }
+        sleep(mt_rand(1, 5));
+
+    }
+
+    sleep(mt_rand(1, 5));
+}
+
+
